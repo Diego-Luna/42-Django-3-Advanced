e6d8341c0680f4e87d84898f8e643c273026d1eb
dluna-lo <dluna-lo@student.42.fr>
2025-11-24 14:30:08 +0100

    ex01 in proseso: Add article detail and publications views with corresponding templates

--- ex00/urls.py
+++ ex00/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ArticlesView, HomeView, LoginUserView
+from .views import ArticlesView, HomeView, LoginUserView, PublicationsView, ArticleDetailView
 
 app_name = 'ex00'
 
 urlpatterns = [
-	path('articles/', ArticlesView.as_view(), name='articles'),
 	path('', HomeView.as_view(), name='home'),
 	path('login/', LoginUserView.as_view(), name='login'),
+	path('articles/', ArticlesView.as_view(), name='articles'),
+	path('publications/', PublicationsView.as_view(), name='publications'),
+	path('detail/<int:pk>/', ArticleDetailView.as_view(), name='detail'),
 ]
--- ex00/views.py
+++ ex00/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, RedirectView, FormView
+from django.views.generic import ListView, RedirectView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -21,3 +22,18 @@ class LoginUserView(LoginView):
 	
 	def get_success_url(self):
 		return reverse_lazy('ex00:home')
+
+class PublicationsView(LoginRequiredMixin, ListView):
+	model = Article
+	template_name = 'ex01/publications.html'
+	context_object_name = 'articles'
+	login_url = '/login/'
+	
+	def get_queryset(self):
+		return Article.objects.filter(author=self.request.user)
+
+class ArticleDetailView(DetailView):
+	model = Article
+	template_name = 'ex01/details.html'
+	context_object_name = 'article'
+	pk_url_kwarg = 'pk'
